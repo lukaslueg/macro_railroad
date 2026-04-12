2f3f459644a5af297785c53e5b5fde741bd6d88c
lukaslueg <lukas.lueg@gmail.com>
2026-04-12 17:25:02 +0200

    cargo fmt

--- benches/to_diagram.rs
+++ benches/to_diagram.rs
@@ -1,4 +1,4 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{Criterion, criterion_group, criterion_main};
 
 // Simple: 3 arms, basic fragments
 const VEC: &str = r#"macro_rules! vec {
